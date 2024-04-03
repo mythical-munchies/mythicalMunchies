5c3561a0363209182aa16ed5c2589f87a4c43a79
Lucas Barrs <l.barrs199@gmail.com>
2024-04-03 11:40:35 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AccountTab from './components/AccountTab';
+import Nav from './components/Nav';
+import ContactTab from './components/ContactTab';
+// import RecipesTab from './components/RecipesTab'
+import HomeTab from './components/HomeTab';
+import SearchBar from './components/SearchBar';
+import CategoryData from './Data.json';
+import Worlds from './components/Worlds';
+import SingleWorld from './components/SingleWorld';
+import SingleRecipe from './components/SingleRecipe';
+import Login from './components/Login';
+
+function App() {
+	return (
+		<>
+			<div className='persistent-header'>
+				<div className='logo-title'>
+					<h2>
+						<img id='logo-image' src='./icons/cauldron yellow.png'></img>{' '}
+						Mythical Munchies
+					</h2>
+				</div>
+				<SearchBar placeholder='Explore Worlds...' data={CategoryData} />
+				{/* <Login /> */}
+			</div>
+			<Nav />
+			<Routes>
+				<Route path='/' element={<HomeTab />} />
+				<Route path='/worlds' element={<Worlds />} />
+				<Route path='/contact' element={<ContactTab />} />
+				{/* ONLY SHOW THE ACCOUNT TAB IF A USER IS LOGGED IN */}
+				<Route path='/account' element={<AccountTab />} />
+				<Route path='/single-world' element={<SingleWorld />} />
+				<Route path='/single-recipe' element={<SingleRecipe />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
