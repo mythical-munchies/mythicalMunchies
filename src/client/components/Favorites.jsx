d826e156a06cd0d161d360e12b9501555139ad91
EleniP94 <eleni.papadatos@gmail.com>
2024-04-04 12:06:49 -0400

    initial favorites commit

---
+++ src/client/components/Favorites.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+// STRETCH GOAL import add to cart if we turn into e-commerce 
+//import remove favorite when it is created
+//STRETCH GOAL importing icons for css 
+
+const favorites = ({ favorites }) => {
+
+    //connect to backend when available. will haver to adjust code when it is available but it should look like:
+    // const {_id, image, title, price, category } = favorites;
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    //removing favorites
+    const removeFavoriteHandler = (favorites) => {
+        dispatch(removeFavorite(favorites));
+    }
+
+    //STRETCH GOAL 
+    //add to cart from favorites page
+
+
+
+return (
+
+<>
+<div className='container'>
+    
+</div>
+</>
+
+)
+
+}
+
+export default favorites;
