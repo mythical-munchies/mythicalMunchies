2cef0dc0fd0108a5e82c054d7dc250f811fc16bc
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-05 12:28:07 -0400

    added social media icons - no link yet

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Container } from "@mui/material";
-
+import { Container, IconButton } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Footer = () => {
   return (
@@ -13,6 +15,15 @@ const Footer = () => {
           <Typography variant="body1" color="inherit">
             Your footer content goes here.
           </Typography>
+          <IconButton color="inherit">
+            <FacebookIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <TwitterIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <InstagramIcon />
+          </IconButton>
         </Toolbar>
       </Container>
     </AppBar>
