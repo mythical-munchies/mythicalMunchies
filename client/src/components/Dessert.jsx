41c6b3bab275fe1bff01514fd9462750f55fd2ef
Samantha Lugtu <sammylugtu@Samanthas-MacBook-Air.local>
2024-04-05 17:21:02 -0400

    finish cleaning

--- client/src/components/Dessert.jsx
+++ client/src/components/Dessert.jsx
@@ -11,10 +11,47 @@ function Dessert() {
 
     <p className="dessert-p">🎂Craving something sweet? Try out these tasty pastries for a boost of mystical powers🦄 </p>
     
-    <p>🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂</p>
-    <hr className="dessert-hr"/>
+    {/* <hr className="dessert-hr"/> */}
 
     {/* map over all recipes with dessert tag and display like all other recipes */}
+    <div className="recipe">
+      <div>
+        {/* this should link the recipe later */}
+        <img className="dessert-img" src={PinkCakee} alt="Stardew Valley Pink Cake" />
+      </div>
+
+      <div className="dessert-info">
+        <h2 className="dessert-title">Recipe Title Link</h2>
+        <p className="dessert-world">World Link</p>
+
+        <p className="dessert-text"> Recipe Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum autem cum perferendis, deleniti, obcaecati sunt iure quia neque consectetur quisquam reiciendis velit iusto accusantium, est quod delectus dignissimos quasi dolorem!</p>
+
+        {/* add onclick to add to bookmarks IF logged in */}
+        <button className="dessert-button">Add to favorites 📕</button>
+      </div>
+    </div>
+
+    <hr className="recipe-hr-dessert"/>
+
+    <div className="recipe">
+      <div>
+        {/* this should link the recipe later */}
+        <img className="dessert-img" src={PinkCakee} alt="Stardew Valley Pink Cake" />
+      </div>
+
+      <div className="dessert-info">
+        <h2 className="dessert-title">Recipe Title Link</h2>
+        <p className="dessert-world">World Link</p>
+
+        <p className="dessert-text"> Recipe Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum autem cum perferendis, deleniti, obcaecati sunt iure quia neque consectetur quisquam reiciendis velit iusto accusantium, est quod delectus dignissimos quasi dolorem!</p>
+
+        {/* add onclick to add to bookmarks IF logged in */}
+        <button className="dessert-button">Add to favorites 📕</button>
+      </div>
+    </div>
+
+    <hr className="recipe-hr-dessert"/>
+
     <div className="recipe">
       <div>
         {/* this should link the recipe later */}
