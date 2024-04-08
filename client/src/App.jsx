fd0232efc89103b54d6f58fc490a3ae481155ec5
Samantha Lugtu <sammylugtu@Samanthas-MacBook-Air.local>
2024-04-08 14:05:47 -0400

    completed drinks page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ import BottomsUp from './components/BottomsUp';
 import Dessert from './components/Dessert';
 import Vegetables from './components/Vegetables';
 import Footer from "./components/Footer";
-// >>>>>>> main
+import Literature from "./components/Literature";
 
 function App() {
   return (
@@ -45,6 +45,7 @@ function App() {
 				<Route path='/bottoms-up' element={<BottomsUp />} />
 				<Route path='/desserts' element={<Dessert />} />
 				<Route path='/veggie-gardens' element={<Vegetables />} />
+        <Route path='/literature' element={<Literature />} />
 
 
         {/* <Route path="/test" element={<Test/>}/> */}
---
+++ client/src/components/Literature.jsx
@@ -0,0 +1,9 @@
+function Literature() {
+  return(
+    <>
+    <h1>These are all of our books!</h1>
+    </>
+  )
+}
+
+export default Literature
