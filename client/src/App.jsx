781a072781348f4b96acaf7d3e2353f661452d44
Samantha Lugtu <sammylugtu@Samanthas-MacBook-Air.local>
2024-04-08 14:21:40 -0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,17 +9,14 @@ import CategoryData from "./Data.json";
 import Worlds from "./components/Worlds";
 import SingleWorld from "./components/SingleWorld";
 import SingleRecipe from "./components/SingleRecipe";
+// import Login from './components/Login'
 import Logo from "./components/icons/cauldronyellow.png";
-import DogWarts from "./components/DogWarts";
-import BottomsUp from "./components/BottomsUp";
-import Dessert from "./components/Dessert";
-import Vegetables from "./components/Vegetables";
+import DogWarts from './components/DogWarts';
+import BottomsUp from './components/BottomsUp';
+import Dessert from './components/Dessert';
+import Vegetables from './components/Vegetables';
 import Footer from "./components/Footer";
 import Literature from "./components/Literature";
-
-import MythicalMunchiesFAQ from "./components/FAQ";
-
-
 
 function App() {
   return (
@@ -50,23 +47,9 @@ function App() {
 				<Route path='/veggie-gardens' element={<Vegetables />} />
         <Route path='/literature' element={<Literature />} />
 
-      <Nav />
-      <Routes>
-        <Route path="/" element={<HomeTab />} />
-        <Route path="/worlds" element={<Worlds />} />
-        <Route path="/contact" element={<ContactTab />} />
-        {/* ONLY SHOW THE ACCOUNT TAB IF A USER IS LOGGED IN - fix later once modules are coded in */}
-        <Route path="/account" element={<AccountTab />} />
-        <Route path="/single-world" element={<SingleWorld />} />
-        <Route path="/single-recipe" element={<SingleRecipe />} />
-        <Route path="/dogwarts" element={<DogWarts />} />
-        <Route path="/bottoms-up" element={<BottomsUp />} />
-        <Route path="/desserts" element={<Dessert />} />
-        <Route path="/veggie-gardens" element={<Vegetables />} />
-        <Route path="/FAQ" element={<MythicalMunchiesFAQ />} />
 
-
-  
+        {/* <Route path="/test" element={<Test/>}/> */}
+        {/* <Route path="/my-reviews" element={<MyReviews/>}/> */}
       </Routes>
       <Footer />
     </>
