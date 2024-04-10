e9d497fc9dae28949fbe9c5d8da0218235ab170f
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-10 16:56:34 -0400

    added loginUser code to users.js

--- server/db/users.js
+++ server/db/users.js
@@ -1,13 +1,18 @@
-const client = require("./client")
-const bcrypt = require("bcrypt")
+const client = require("./client");
+const bcrypt = require("bcrypt");
 
-const createUser = async ({username, email, password}) => {
-  const SQL =`
+const createUser = async ({ username, email, password }) => {
+  const SQL = `
   INSERT INTO users(id, username, email, password)
   VALUES($1, $2, $3, $4)
   RETURNING *
   `;
-  const response = await client.query(SQL, [uuid.v4(), username, email, await bcrypt.hash(password, 5)]);
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    email,
+    await bcrypt.hash(password, 5),
+  ]);
   return response.rows[0];
 };
 
@@ -16,11 +21,17 @@ const createUserRecipe = async (user_id, recipe_id, rating, review) => {
   INSERT INTO user_recipe(id, user_id, recipe_id, rating, review)
   VALUES($1, $2, $3, $4, $5)
   `;
-  const response = await client.query(SQL, [uuid.v4(), user_id, recipe_id, rating, review])
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    user_id,
+    recipe_id,
+    rating,
+    review,
+  ]);
   return response.rows[0];
 };
 
-const fetchUsers = async() => {
+const fetchUsers = async () => {
   const SQL = `
     SELECT *
     FROM users
@@ -29,13 +40,13 @@ const fetchUsers = async() => {
   return response.rows;
 };
 
-const fetchUser = async(user_id) => {
+const fetchUser = async (user_id) => {
   const SQL = `
     SELECT *
     FROM users
     WHERE user_id = $1
   `;
-  const response = await client.query(SQL, [ user_id ]);
+  const response = await client.query(SQL, [user_id]);
   return response.rows;
 };
 
@@ -50,6 +61,17 @@ const fetchUserByEmailOrUsername = async (emailOrUsername) => {
   return response.rows;
 };
 
+const loginUser = async ({ emailOrUsername, password }) => {
+  // Query the database to find the user based on email or username
+  const user = await fetchUserByEmailOrUsername(emailOrUsername);
+  // If user is not found or password doesn't match, throw an error
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    throw new Error("Invalid credentials");
+  }
+  return user;
+};
+
+//End of Simone's testing code
 
 const fetchUserRecipes = async (user_id) => {
   const SQL = `
@@ -57,28 +79,30 @@ const fetchUserRecipes = async (user_id) => {
   FROM user_recipe
   WHERE user_id = $1
 `;
-const response = await client.query(SQL, [ user_id ]);
-return response.rows;
+  const response = await client.query(SQL, [user_id]);
+  return response.rows;
 };
 
-const deleteUserRecipe = async({ user_id, id}) => {
+const deleteUserRecipe = async ({ user_id, id }) => {
   const SQL = `
     DELETE FROM user_recipe
     WHERE id = $1 AND user_id = $2
     RETURNING *
   `;
-  const response = await client.query(SQL, [ id, user_id ]);
-  if(!response.rows.length){
-    const error = Error('no review found');
+  const response = await client.query(SQL, [id, user_id]);
+  if (!response.rows.length) {
+    const error = Error("no review found");
     error.status = 500;
     throw error;
   }
-}
+};
 module.exports = {
   createUser,
   createUserRecipe,
-  fetchUsers, 
+  fetchUsers,
   fetchUser,
-  fetchUserRecipes, 
-  deleteUserRecipe
-}
+  fetchUserRecipes,
+  deleteUserRecipe,
+  fetchUserByEmailOrUsername,
+  loginUser,
+};
