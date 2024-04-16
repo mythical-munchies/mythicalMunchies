110d945e4107530a3353fe4cf3504a7497868cfc
SammyLug <samlugtu777@gmail.com>
2024-04-16 13:10:46 -0400

    cleanup cleanup everybody cleanup

--- client/src/components/LeaveReview.jsx
+++ client/src/components/LeaveReview.jsx
@@ -32,11 +32,11 @@ function LeaveReview() {
         <form className="p-4 md:p-5">
                       <div className="grid gap-4 mb-4 grid-cols-2">
                           <div className="col-span-2">
-                              <label id='input-text' for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username:</label>
+                              <label id='input-text' htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username:</label>
                               <input type="text" name="name" id="review-modal-text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave empty to stay anonymous" required="" />
                           </div>
                           <div className="col-span-2 sm:col-span-1">
-                              <label id='input-text' for="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Recipe:</label>
+                              <label id='input-text' htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Recipe:</label>
                               <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                 {/* this should later be coded as {recipe.name} */}
                                   <option selected="">Ponyo Ham Ramen</option>
@@ -47,7 +47,7 @@ function LeaveReview() {
                         
 
                           <div className="col-span-2">
-                              <label id='input-text' for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Review</label>
+                              <label id='input-text' htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Review</label>
                               <textarea id="review-modal-text" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Start typing..."></textarea>                    
                           </div>
                       </div>
@@ -68,7 +68,7 @@ function LeaveReview() {
           </button>
 
           <button id="submit" type="submit" className="text-white inline-flex items-center bg-red-900 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+            <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
             Submit Review
           </button>
           {/* <Button variant="primary">Understood</Button> */}
--- client/src/components/MyReviews.jsx
+++ client/src/components/MyReviews.jsx
@@ -8,105 +8,83 @@ function MyReviews() {
   return(
     <>
     <div className="largest-contain">
-          <div className="main-contain">  
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-             <span className="sr-only">Open sidebar</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-                  </path>
-                </svg>
-            </button>
+      <div className="main-contain">  
+        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+          <span className="sr-only">Open sidebar</span>
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+          </svg>
+        </button>
 
-            {/* content should only be shown if login AUTH */}
+{/* content should only be shown if login AUTH */}
 
 {/* THIS IS THE ACCOUNT CONTENT */}
-            <div className="testicle">
-              <div className="testin">
-              <h2 className="bookmark-title">We appreciate your feedback. Let us know what you love!</h2>
-                <div className="reviewed">
-                  {mockReviews.map((review) => {
-                    return (
-                      <div>
-                        {/* just using mock data but will map over favorites */}
-                        <div key={review.id} className="review-row">
+        <div className="testicle">
+          <div className="testin">
+            <h2 className="bookmark-title">We appreciate your feedback. Let us know what you love!</h2>
+              <div className="reviewed">
+                {mockReviews.map((review) => {
+                  return (
+                    <div>
+                      {/* just using mock data but will map over favorites */}
+                      <div key={review.id} className="review-row">
                         <a href="somesite.com" className="review-recipe-title">{review.title}</a>
-                        <p className="review-date">{review.day}</p>
-                        {/* <button className="delete-review">Delete</button> */}
-                        <DeleteReview />
-                        <hr />
-                        </div>
+                          <p className="review-date">{review.day}</p>
+                          {/* delete review Modal */}
+                          <DeleteReview />
+                          <hr />
                       </div>
-                    );
-                  })}
-                </div>
-            
+                    </div>
+                  );
+                })}
               </div>
-            </div>
-
-{/* THIS IS THE SIDEBAR */}
+          </div>
+        </div>
+        
+{/* THIS IS THE PERSISTENT SIDEBAR */}
             <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
               <div id="sideBar" className="h-full px-3 py-4 overflow-y-auto bg-white-50 dark:bg-gray-800">
-
                 <div>
                   <h1 className="welcome-user">Welcome Back username</h1>
                 </div>
-
                 <span className="logout-container">
                   <button className="logout-button">Logout</button>
                 </span>
 
                 <ul className="space-y-2 font-medium">
-                    <li className="sideBar-button">
-                      <a href="/account" className="flex items-center p-2 text-blue-900 rounded-lg dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
-                        <svg className="w-[30px] h-[30px] text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                          <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
-                        </svg>
-                        <span id="side-bar-texty" className="flex-1 ms-3 whitespace-nowrap">Account Details
-                        </span>
-                      </a>
-                    </li>
+                  <li className="sideBar-button">
+                    <a href="/account" className="flex items-center p-2 text-blue-900 rounded-lg dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
+                      <svg className="w-[30px] h-[30px] text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
+                      </svg>
+                      <span id="side-bar-texty" className="flex-1 ms-3 whitespace-nowrap">Account Details</span>
+                    </a>
+                  </li>
 
-                    <li className="sideBar-button">
-                      <a href="/bookmarks" className="flex items-center p-2 text-blue-900 rounded-lg   dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
-                        <svg className="w-[30px] h-[30px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <li className="sideBar-button">
+                    <a href="/bookmarks" className="flex items-center p-2 text-blue-900 rounded-lg   dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
+                      <svg className="w-[30px] h-[30px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z"/>
-                        </svg>
-                        <span className="flex-1 ms-3 whitespace-nowrap">Bookmarks</span>
-                      </a>
-                    </li>
+                      </svg>
+                      <span className="flex-1 ms-3 whitespace-nowrap">Bookmarks</span>
+                    </a>
+                  </li>
 
-                    <li className="sideBar-button">
-                      <a href="/my-reviews" className="flex items-center p-2 text-blue-900 rounded-lg dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
-                        <svg className="w-[30px] h-[30px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
-                        </svg>
-                        <span className="flex-1 ms-3 whitespace-nowrap">Reviews</span>
-                      </a>
-                    </li>
-              
-                    {/* <li className="sideBar-button">
-                      <a href="/login" className="flex items-center p-2 text-blue-900 rounded-lg dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
-                        <svg className="w-[30px] h-[30px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.6" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
-                        </svg>
-                        <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-                      </a>
-                    </li> */}
-                    <Login />
-
-                    {/* <li className="sideBar-button">
-                      <a href="/signup" className="flex items-center p-2 text-blue-900 rounded-lg dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
-                        <svg className="w-[30px] h-[30px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                          <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.495.93A.5.5 0 0 0 6.5 13c0 1.19.644 2.438 1.618 3.375C9.099 17.319 10.469 18 12 18c1.531 0 2.9-.681 3.882-1.625.974-.937 1.618-2.184 1.618-3.375a.5.5 0 0 0-.995-.07.764.764 0 0 1-.156.096c-.214.106-.554.208-1.006.295-.896.173-2.111.262-3.343.262-1.232 0-2.447-.09-3.343-.262-.452-.087-.792-.19-1.005-.295a.762.762 0 0 1-.157-.096ZM8.99 8a1 1 0 0 0 0 2H9a1 1 0 1 0 0-2h-.01Zm6 0a1 1 0 1 0 0 2H15a1 1 0 1 0 0-2h-.01Z" clipRule="evenodd"/>
-                        </svg>
-                        <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-                      </a>
-                    </li> */}
-                    <SignUp />
-                  </ul>
+                  <li className="sideBar-button">
+                    <a href="/my-reviews" className="flex items-center p-2 text-blue-900 rounded-lg dark:text-white hover:bg-yellow-400 dark:hover:bg-yellow-800 group">
+                      <svg className="w-[30px] h-[30px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
+                      </svg>
+                      <span className="flex-1 ms-3 whitespace-nowrap">Reviews</span>
+                    </a>
+                  </li>
+                  {/* import signup modal - these function when the sidebar button is clicked  */}  
+                  <Login /> 
+                  {/* import signup modal - these function when the sidebar button is clicked  */}
+                  <SignUp />
+                </ul>
               </div>
             </aside>
-            
         </div>
       </div>
     </>
@@ -115,30 +93,3 @@ function MyReviews() {
 
 export default MyReviews
 
-
-{/* <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-Toggle modal
-</button>
-
-<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-4 md:p-5 text-center">
-                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                    Yes, I'm sure
-                </button>
-                <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
-            </div>
-        </div>
-    </div>
-</div> */}
