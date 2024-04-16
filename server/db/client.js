c02b8315e6dfbd1521aed16106ae0c624785e3ad
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-16 11:55:56 -0400

    troubleshooting solve with Anais

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,12 @@
-const { Client } = require('pg')
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost/mythical_munchies_db');
+const { Client, DatabaseError } = require("pg");
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/mythical_munchies_db";
+const client = new Client({
+  connectionString: DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 module.exports = client;
