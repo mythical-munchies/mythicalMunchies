22b6db1bf59b3341f1e89c79dd72e838c16c95c5
SammyLug <samlugtu777@gmail.com>
2024-04-17 12:06:11 -0400

    trying react-bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import AccountTab from "./components/AccountTab";
