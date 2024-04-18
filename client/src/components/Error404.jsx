b5490bd2fb3e81eecef7afe648fc7f52afe46cea
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-18 16:03:06 -0400

    updated App.jsx file

---
+++ client/src/components/Error404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Error404() {
+  return (
+    <div>
+      404 Magic Munchies missing! Looks like it vanished into thin air. Let's
+      conjure up another dish instead!" ✨🍽️!
+    </div>
+  );
+}
+
+export default Error404;
