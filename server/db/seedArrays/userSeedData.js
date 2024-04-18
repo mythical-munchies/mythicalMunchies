0bc95188fec8c90f42f8b0e5346c6ed86b78f75b
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-18 17:42:29 -0400

    added three profiles

--- server/db/seedArrays/userSeedData.js
+++ server/db/seedArrays/userSeedData.js
@@ -2,27 +2,47 @@ const users = [
   {
     username: "gandalf",
     email: "gandalf@gmail.com",
-    password: "sauroniswack"
+    password: "sauroniswack",
   },
   {
     username: "MHobbs",
     email: "hob54@gmail.com",
-    password: "freakonomics"
+    password: "freakonomics",
   },
   {
     username: "magicat",
     email: "magic@gmail.com",
-    password: "catcat"
+    password: "catcat",
   },
   {
     username: "test",
     email: "test@gmail.com",
-    password: "testier"
+    password: "testier",
   },
   {
     username: "MMAdmin",
     email: "mythicalmunchies@gmail.com",
-    password: "GHP2401"
+    password: "GHP2401",
+  },
+  {
+    username: "Totoro_Fan",
+    email: "totorofan@gmail.com",
+    password: "spiritedaway",
+  },
+  {
+    username: "Brienne_is_Best",
+    email: "brienne@gmail.com",
+    password: "oathkeeper",
+  },
+  {
+    username: "Hobbiton_Homebody",
+    email: "shirefolk@middleearth.com",
+    password: "secondbreakfast",
+  },
+  {
+    username: "Fairy_Oddparent",
+    email: "fairyoddparent@gmail.com",
+    password: "booboo",
   },
 ];
 
