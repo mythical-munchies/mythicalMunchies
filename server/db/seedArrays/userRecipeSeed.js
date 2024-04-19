17741ff5d86f13593f91c08e04bb7bc9b15314e4
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-19 14:08:26 -0400

    brienne review

--- server/db/seedArrays/userRecipeSeed.js
+++ server/db/seedArrays/userRecipeSeed.js
@@ -1,32 +1,57 @@
 const user_recipes = [
   {
-  username:"MHobbs",
-  recipe_name: "Hot Honey Milk",
-  rating: "5",
-  review: "This such a warm and cozy drink!",
-  bookmarked: "true"
+    username: "MHobbs",
+    recipe_name: "Hot Honey Milk",
+    rating: "5",
+    review: "This such a warm and cozy drink!",
+    bookmarked: "true",
   },
   {
-    username:"MHobbs",
+    username: "MHobbs",
     recipe_name: "Sophie's Tea",
     rating: "5",
     review: "A great pairing for a cold day",
-    bookmarked: "true"
+    bookmarked: "true",
   },
   {
-    username:"MHobbs",
+    username: "MHobbs",
     recipe_name: "Bacon and Eggs",
     rating: "5",
     review: "A great breakfast to share",
-    bookmarked: "false"
+    bookmarked: "false",
   },
   {
-    username:"gandalf",
+    username: "gandalf",
     recipe_name: "Sophie's Tea",
     rating: "5",
     review: "Perfect for studying",
-    bookmarked: "false"
+    bookmarked: "false",
   },
+  {
+    username: "MHobbs",
+    recipe_name: "Pumpkin Pasties",
+    rating: "4",
+    review:
+      "Absolutely magical! These pumpkin pasties brought the enchanting flavors of Hogwarts right to my kitchen. The spiced pumpkin filling was perfectly balanced, and the pastry was flaky and delicious. While they were a bit time-consuming to make, the end result was well worth it. My family couldn't get enough of them! Definitely a recipe worth bookmarking for special occasions or when you're craving a taste of the Wizarding World.",
+    bookmarked: "true",
+  },
+  {
+    username: "magicat",
+    recipe_name: "Hot Honey Milk",
+    rating: "5",
+    review:
+      "Meowgical! This hot honey milk whisked me away to the cozy scenes of Studio Ghibli films. It's like a warm hug on a rainy day, soothing both body and soul. The honey adds a delightful touch of sweetness, perfectly complementing the creamy milk. And the best part? It takes just 5 minutes to make, so you can enjoy its comforting embrace whenever you need a little pick-me-up. Definitely keeping this recipe close by for those purrfectly relaxing evenings.",
+    bookmarked: "true",
+  },
+  {
+    username: "Brienne_is_Best",
+    recipe_name: "Tyrion's Mulled Wine",
+    rating: "5",
+    review:
+      "As sturdy as Oathkeeper and as comforting as a hearthfire in Winterfell. Tyrion's Mulled Wine is a true delight for any noble or commoner alike. The blend of spices creates a symphony of flavors that warms the heart and lifts the spirits. Whether you're braving the cold nights of King's Landing or enjoying a feast in the Great Hall of the Red Keep, this mulled wine is a must-have companion. It's easy to prepare in just 30 minutes, making it perfect for impromptu gatherings or quiet evenings by the fire. I highly recommend it to anyone seeking a taste of Westerosian indulgence.",
+    bookmarked: "true",
+  },
+
   // {
   //   username:"",
   //   recipe_name: "",
@@ -34,6 +59,6 @@ const user_recipes = [
   //   review: "",
   //   bookmarked: ""
   // },
-]
+];
 
-module.exports = {user_recipes}
+module.exports = { user_recipes };
