19bb875578d1b64f74fd974abbbcda67bd6f384e
Simone LaPray <simonelapray@Simones-MacBook-Pro.local>
2024-04-19 08:33:32 -0400

    updated the seed tags for Zelda

--- server/db/seedArrays/recipeTagsSeed.js
+++ server/db/seedArrays/recipeTagsSeed.js
@@ -31,6 +31,15 @@ const recipe_tags = [
     recipe_name: "Jon Snow's Wildling Stew",
     description: "Entree"
   },
+  {
+  recipe_name: "Lon Lon Milkshake",
+  description: "Creamy and refreshing milkshake inspired by Lon Lon Ranch from The Legend of Zelda series. Made with fresh milk, vanilla ice cream, and a touch of nostalgia, it's the perfect treat for adventurers seeking a moment of respite on their journey."
+},
+{
+  recipe_name: "Triforce Sugar Cookies",
+  description: "Delicious sugar cookies inspired by the iconic Triforce symbol from The Legend of Zelda series. These cookies are buttery, sweet, and perfect for any gaming-themed gathering or snack time adventure."
+},
+
   
 
 
